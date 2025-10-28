f39a143330ecaf3af4ed6e28db0b99248fe9c71e
Carla Montano <cm.emprendiendo@gmail.com>
2025-10-27 23:33:57 -0400

    receiving chunks with langchain

--- server.mjs
+++ server.mjs
@@ -1,23 +1,22 @@
 import express from "express";
 import { ChatOllama } from "@langchain/ollama";
+import cors from 'cors';
 
 const app = express();
 
-const model = new ChatOllama({
-  model: 'llama3.2'
+app.use(cors());
+app.use(express.json());
+
+const chatModel = new ChatOllama({
+model: 'llama3.2'
 });
-
-app.get('/', async (request, response) => {
-  response.type('text/plain');
-
-  const modelResponse = 
-    await model.invoke(
-      "Can you simply say 'test'?");
-
-  console.log("\nAIMessage object response:\n")
-  console.log(modelResponse);
-
-  response.send(modelResponse.content);
+app.post('/', async (request, response) => {
+const streamIterator = await chatModel.stream("Can you tell me about the " +
+"Beatles in 500 words or less?");
+for await (const chunk of streamIterator) {
+response.write(chunk.content);
+}
+response.end();
 });
 
 app.listen(8000, () => {
